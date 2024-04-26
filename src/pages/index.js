40cb8093adb2b746424fcfabfe3f799bf83c0954
pascoot22 <pscott1@terpmail.umd.edu>
2024-04-26 11:34:22 -0400

    removed background image

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,6 @@
 import * as React from 'react'
 import Layout from '../components/layout'
 import { StaticImage } from 'gatsby-plugin-image'
-import BackgroundImage from 'gatsby-background-image'
 
 const IndexPage = () => {
   return (
